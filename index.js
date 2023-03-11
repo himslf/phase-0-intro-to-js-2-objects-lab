c717b659ad3ff6add0db298709fe603d0aaa19d4
JASON YOUNG <moorslf@gmail.com>
2023-03-11 09:21:31 -0500

    Objects Lab Submission

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Penelope",
+    streetAddress : "7 World Trade Center",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key] : value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    // spread operator
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+};
